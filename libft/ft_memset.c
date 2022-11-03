79261d111afc8f7c19ae11832d769a8303479e49
Fabio <fabiovv@hotmail.com>
2022-11-03 15:52:28 +0000

    libft_03_11

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -1,8 +1,11 @@
 #include<stdlib.h>
 void	*ft_memset(void *s, int c, size_t n)
 {
+	size_t	i;
+	
+	i = 0;
 	while (n--)
-		*((unsigned char *)s++) = c;
+		((unsigned char *)s)[i++] = c;
 	return (s);
 }
 #include<stdio.h>
@@ -11,9 +14,7 @@ int	main(void)
 {
 	char	name[] = "fabio valli vieira";
 	char	name1[] = "fabio valli vieira";
-	ft_memset(name, 1150, 5);
-//	memset(name1, 1150, 5);
-	printf("%s\n",name);
-	printf("%s\n",(unsigned char*)ft_memset(name1, 1150, 5));
+	printf("%s\n",(unsigned char*)ft_memset(name, 0, 5));
+	printf("%s\n",(unsigned char*)ft_memset(name1, 0, 5));
 	return (0);
 }
