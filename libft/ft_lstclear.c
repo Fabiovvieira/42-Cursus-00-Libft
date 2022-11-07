5df13fc9c34c460b6839bdd8299d54faff13f81c
Fabio Valli Vieira <fvalli-v@DESKTOP-1SKH6HJ.localdomain>
2022-11-07 22:54:19 +0000

    ft_lstclear.c and some corrections

--- libft/ft_lstclear.c
+++ libft/ft_lstclear.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstclear.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fvalli-v <fvalli-v@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 13:30:07 by fvalli-v          #+#    #+#             */
-/*   Updated: 2022/11/07 17:01:29 by fvalli-v         ###   ########.fr       */
+/*   Updated: 2022/11/07 22:51:55 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,15 @@
 
 void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
+	t_list	*next;
+
 	if (*lst != NULL && del != NULL)
 	{
 		while (*lst)
 		{
-			del((*lst)->content);
-			free (*lst);
-			*lst = (*lst)->next;
+			next = (*lst)->next;
+			ft_lstdelone(*lst, del);
+			*lst = next;
 		}
 	}
 }
